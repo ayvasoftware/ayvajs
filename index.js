ddd87abd7a5c26b95742d9c312f9d899ccbd773c
soritesparadox <48735681+soritesparadox@users.noreply.github.com>
2022-01-20 12:52:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export { default as Ayva } from './src/ayva.js';
+export { default } from './src/ayva.js';
 export { default as AyvaMoveBuilder } from './src/ayva-move-builder.js';
 export { default as AyvaBehavior } from './src/behaviors/ayva-behavior.js';
