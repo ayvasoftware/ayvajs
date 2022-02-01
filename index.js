504c752edeb8f7ba5c1d1542afb9f3d01920baf1
soritesparadox <48735681+soritesparadox@users.noreply.github.com>
2022-02-01 15:28:04 -0500

    Implement BrowserSerialDevice

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export { default } from './src/ayva.js';
 export { default as AyvaBehavior } from './src/behaviors/ayva-behavior.js';
 export { default as ClassicStroke } from './src/behaviors/classic-stroke.js';
 export { default as TempestStroke } from './src/behaviors/tempest-stroke.js';
+export { default as BrowserSerialDevice } from './src/devices/browser-serial-device.js';
---
+++ src/devices/browser-serial-device.js
@@ -0,0 +1,61 @@
+/**
+ * Small convenience class for easily connecting to a serial device from a browser.
+ * This class has been excluded from test coverage because mocking all the browser
+ * globals involved is cumbersome. But it works. Trust me :).
+ */
+class BrowserSerialDevice {
+  #baudRate;
+
+  #connected = false;
+
+  #output = null;
+
+  #input = null;
+
+  #serial = null;
+
+  get connected () {
+    return this.#connected;
+  }
+
+  constructor (baudRate = 115200, serial = null) {
+    this.#baudRate = baudRate;
+    this.#serial = serial || (globalThis.navigator ? globalThis.navigator.serial : null);
+  }
+
+  async requestConnection () {
+    const port = await this.#serial.requestPort();
+
+    await port.open({ baudRate: this.#baudRate });
+
+    const encoder = new TextEncoderStream();
+    encoder.readable.pipeTo(port.writable);
+
+    const decoder = new TextDecoderStream();
+    port.readable.pipeTo(decoder.writable);
+
+    this.#output = encoder.writable.getWriter();
+    this.#input = decoder.readable.getReader();
+    this.#connected = true;
+
+    const disconnectListener = (event) => {
+      if (event.target === port) {
+        this.#connected = false;
+
+        this.#serial.removeEventListener('disconnect', disconnectListener);
+      }
+    };
+
+    this.#serial.addEventListener('disconnect', disconnectListener);
+  }
+
+  write (output) {
+    if (this.#connected) {
+      this.#output.write(output);
+    } else {
+      throw new Error('No device connected.');
+    }
+  }
+}
+
+export default BrowserSerialDevice;
